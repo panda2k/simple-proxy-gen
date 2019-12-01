d345b5a514d9115a8a4841aacbaa3eba814935d9
panda2k <thelowkeypanda@gmail.com>
2019-12-01 10:11:06 -0800

    Add create proxy method and remove get stack script id

--- linodeproxygen.py
+++ linodeproxygen.py
@@ -3,6 +3,7 @@ import requests
 import json
 import haikunator
 import proxymodels
+from datetime import datetime
 
 class LinodeProxygen():
     access_token = None
@@ -11,7 +12,7 @@ class LinodeProxygen():
     def __init__(self):
         self.access_token = os.environ.get('LINODE_ACCESS_TOKEN')
     
-    def create_server(self, location, proxy_username, proxy_password):
+    def create_server(self, location, proxy_username, proxy_password, stack_script_id):
         name_gen = haikunator.Haikunator()
         headers = {
             "Authorization": f"Bearer {self.access_token}",
@@ -23,8 +24,8 @@ class LinodeProxygen():
             "backups_enabled": False,
             "booted": True,
             "image": "linode/ubuntu16.04lts",
-            "root_pass": name_gen.haikunate()
-            #"stackscript_id": ''
+            "root_pass": name_gen.haikunate(),
+            "stackscript_id": stack_script_id
         }
         response = json.loads(requests.post(self.base_url + '/linode/instances', json.dumps(server), headers=headers).text)
 
@@ -50,19 +51,9 @@ class LinodeProxygen():
             "is_public": False,
             "script": startup_script 
         }
-        response = requests.post(self.base_url + '/linode/stackscripts', json.dumps(request), headers=headers)
-
-    def get_stack_script_id(self, stack_script_name):
-        headers = {
-            "Authorization": f"Bearer {self.access_token}"
-        }
-        print(headers)
-        response = requests.get(self.base_url + '/linode/stackscripts', headers=headers)
-        print(response.text)
-        print(response)
-        #print(json.loads(response.text))
-        #for x in json.loads(response.text)['data']:
-        #    print(x['label'])
+        response = json.loads(requests.post(self.base_url + '/linode/stackscripts', json.dumps(request), headers=headers).text)
+        
+        return response['id']
 
     def get_startup_script(self, file_name, proxy_username_identifier,  proxy_username, proxy_password_identifier, proxy_password):
         startup_script_file = open(file_name, "r")
@@ -72,13 +63,29 @@ class LinodeProxygen():
         startup_script = startup_script.replace(proxy_password_identifier, proxy_password)
 
         return startup_script
+    
+    def create_proxies(self, location, proxy_count, proxy_username, proxy_password):
+        servers = []
+        stack_script_name = 'proxy-setup' + str(datetime.now())
+        startup_script = self.get_startup_script('proxystartupscript', 'username', proxy_username, 'password', proxy_password)
+        stack_script_id = self.create_stack_script(startup_script, stack_script_name)
+        for x in range(proxy_count):
+            servers.append(self.create_server(location, proxy_username, proxy_password, stack_script_id))
+        
+        return servers
 
 def main():
     proxy_gen = LinodeProxygen()
-    startup_script = proxy_gen.get_startup_script('proxystartupscript', 'username', 'tester', 'password', 'testpass')
+    #startup_script = proxy_gen.get_startup_script('proxystartupscript', 'username', 'tester', 'password', 'testpass')
     #proxy_gen.create_stack_script(startup_script, 'proxysetup')
     #print(proxy_gen.get_stack_script_id('proxysetup'))
-    proxy_gen.create_server("us-east")
+    #proxy_gen.create_server("us-east")
+    servers = proxy_gen.create_proxies('us-east', 3, 'testuser', 'testpassword')
+    for x in servers:
+        print(x.to_string())
+    input("enter anything to delete proxies")
+    for x in servers:
+        proxy_gen.delete_server(x.server_id)
 
 if __name__ == "__main__":
     main()
