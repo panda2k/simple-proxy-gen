a1a9a9f3511b30819f34afd703fdbb970279e6b3
panda2k <thelowkeypanda@gmail.com>
2019-12-08 12:33:32 -0800

    Add create_proxies method
    Also updating create_vm and get_vm_ip_address

--- googlecloudproxygen.py
+++ googlecloudproxygen.py
@@ -1,5 +1,7 @@
 import googleapiclient.discovery
 import haikunator
+import time
+import proxymodels
 
 class GoogleCloudProxyGen:
     compute_client = None
@@ -7,12 +9,12 @@ class GoogleCloudProxyGen:
     def __init__(self):
         self.compute_client = googleapiclient.discovery.build('compute', 'v1')
     
-    def create_vm(self, project, zone, startup_script_location, username_identifier, username, password_identifier, password):
+    def create_vm(self, project, zone, startup_script):
         name_gen = haikunator.Haikunator()
         image_response = self.compute_client.images().getFromFamily(project='ubuntu-os-cloud', family='ubuntu-1604-lts').execute()
-        startup_script = self.get_startup_script(startup_script_location, username_identifier, username, password_identifier, password)
+        vm_name = name_gen.haikunate()
         server = {
-            'name': name_gen.haikunate(),
+            'name': vm_name,
             'machineType': f"zones/{zone}/machineTypes/n1-standard-1",
             'disks': [
                 {
@@ -41,12 +43,19 @@ class GoogleCloudProxyGen:
             }
         }
         creation_result = self.compute_client.instances().insert(project = project, zone=zone, body=server).execute()
+        return vm_name
 
-
-
-    def delete_vm(self, project, zone, name):
-        return self.compute_client.instances().delete(project=project, zone=zone, instance=name).execute()
+    def delete_vm(self, project, zone, server_name):
+        return self.compute_client.instances().delete(project=project, zone=zone, instance=server_name).execute()
     
+    def get_vm_ip_address(self, project, zone, server_name):
+        while(True):
+            try:
+                server_ip = self.compute_client.instances().get(project=project, zone=zone, instance=server_name).execute()['networkInterfaces'][0]['accessConfigs'][0]['natIP']
+                return server_ip
+            except KeyError as identifier:
+                time.sleep(5)
+
     def get_startup_script(self, startup_script_location, username_identifier, username, password_identifier, password):
         startup_script_file = open(startup_script_location, "r")
         startup_script = startup_script_file.read()
@@ -54,11 +63,25 @@ class GoogleCloudProxyGen:
         startup_script = startup_script.replace(password_identifier, password)
 
         return startup_script
+    
+    def create_proxies(self, project, region, proxy_count, proxy_username, proxy_password):
+        startup_script = self.get_startup_script('proxystartupscript', 'username', proxy_username, 'password', proxy_password)
+        servers = []
+        for x in range(proxy_count):
+            server_name = self.create_vm(project, region, startup_script)
+            server_ip = self.get_vm_ip_address(project, region, server_name)
+            servers.append(proxymodels.GoogleCloudServer(project, region, server_name, server_ip, proxy_username, proxy_password, 80))
+        
+        return servers
 
 def main():
     proxy_gen = GoogleCloudProxyGen()
-    #proxy_gen.create_vm('focal-maker-240918', 'us-east4-c', 'proxystartupscript', 'username', 'testuser', 'password', 'testpass')
-    proxy_gen.delete_vm('focal-maker-240918', 'us-east4-c', 'divine-base-8146')
+    servers = proxy_gen.create_proxies('focal-maker-240918', 'us-east4-c', 5, 'testuser', 'testpass')
+    for x in servers:
+        print(x.to_string())
+    input('type anything to delete')
+    for x in servers:
+        proxy_gen.delete_vm('focal-maker-240918', 'us-east4-c', x.server_name)
 
 if __name__ == "__main__":
     main()
