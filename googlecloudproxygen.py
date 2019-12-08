4be97ddcd97cb4d31cbf54ec298cf20af42eb337
panda2k <thelowkeypanda@gmail.com>
2019-12-08 12:48:39 -0800

    remove project parameter

--- googlecloudproxygen.py
+++ googlecloudproxygen.py
@@ -1,15 +1,18 @@
 import googleapiclient.discovery
 import haikunator
+import os
 import time
 import proxymodels
 
 class GoogleCloudProxyGen:
     compute_client = None
+    project = None
     
     def __init__(self):
         self.compute_client = googleapiclient.discovery.build('compute', 'v1')
+        self.project = os.environ.get('GOOGLE_CLOUD_PROJECT')
     
-    def create_vm(self, project, zone, startup_script):
+    def create_vm(self, zone, startup_script):
         name_gen = haikunator.Haikunator()
         image_response = self.compute_client.images().getFromFamily(project='ubuntu-os-cloud', family='ubuntu-1604-lts').execute()
         vm_name = name_gen.haikunate()
@@ -42,16 +45,16 @@ class GoogleCloudProxyGen:
                 ]
             }
         }
-        creation_result = self.compute_client.instances().insert(project = project, zone=zone, body=server).execute()
+        creation_result = self.compute_client.instances().insert(project=self.project, zone=zone, body=server).execute()
         return vm_name
 
-    def delete_vm(self, project, zone, server_name):
-        return self.compute_client.instances().delete(project=project, zone=zone, instance=server_name).execute()
+    def delete_vm(self, zone, server_name):
+        return self.compute_client.instances().delete(project=self.project, zone=zone, instance=server_name).execute()
     
-    def get_vm_ip_address(self, project, zone, server_name):
+    def get_vm_ip_address(self, zone, server_name):
         while(True):
             try:
-                server_ip = self.compute_client.instances().get(project=project, zone=zone, instance=server_name).execute()['networkInterfaces'][0]['accessConfigs'][0]['natIP']
+                server_ip = self.compute_client.instances().get(project=self.project, zone=zone, instance=server_name).execute()['networkInterfaces'][0]['accessConfigs'][0]['natIP']
                 return server_ip
             except KeyError as identifier:
                 time.sleep(5)
@@ -64,13 +67,13 @@ class GoogleCloudProxyGen:
 
         return startup_script
     
-    def create_proxies(self, project, region, proxy_count, proxy_username, proxy_password):
+    def create_proxies(self, region, proxy_count, proxy_username, proxy_password):
         startup_script = self.get_startup_script('proxystartupscript', 'username', proxy_username, 'password', proxy_password)
         servers = []
         for x in range(proxy_count):
-            server_name = self.create_vm(project, region, startup_script)
-            server_ip = self.get_vm_ip_address(project, region, server_name)
-            servers.append(proxymodels.GoogleCloudServer(project, region, server_name, server_ip, proxy_username, proxy_password, 80))
+            server_name = self.create_vm(region, startup_script)
+            server_ip = self.get_vm_ip_address(region, server_name)
+            servers.append(proxymodels.GoogleCloudServer(region, server_name, server_ip, proxy_username, proxy_password, 80))
         
         return servers
 
